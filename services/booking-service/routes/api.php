76ce8df125f721a3380b54fe7a6466fba3e105f2
Taraka Zubair Gama <tarakazubair@gmail.com>
2026-05-01 21:01:43 +0700

    feat(booking): booking api

--- services/booking-service/routes/api.php
+++ services/booking-service/routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookingController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+// Endpoint untuk melihat semua daftar booking
+Route::get('/bookings', [BookingController::class, 'index']);
+
+// Endpoint untuk membuat booking baru
+Route::post('/bookings', [BookingController::class, 'store']);
+
+// Endpoint untuk membatalkan/menghapus booking
+Route::delete('/bookings/{id}', [BookingController::class, 'destroy']);
