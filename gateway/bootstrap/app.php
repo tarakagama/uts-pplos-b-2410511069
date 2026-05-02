8562fa6853df77345f559d1ec7ea2193316b4a84
Taraka Zubair Gama <tarakazubair@gmail.com>
2026-05-02 14:19:13 +0700

    feat: jwt validation and rate limiting

--- gateway/bootstrap/app.php
+++ gateway/bootstrap/app.php
@@ -11,9 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->alias([
+            'jwt.gateway' => \App\Http\Middleware\JwtGatewayMiddleware::class,
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
--- gateway/routes/api.php
+++ gateway/routes/api.php
@@ -4,21 +4,35 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 
-// --- 1. PROXY KE AUTH SERVICE ---
-Route::get('/auth/github', function () {
-    return Http::get(env('AUTH_SERVICE_URL') . '/api/login/github')->json();
-});
 
-// --- 2. PROXY KE FIELD SERVICE ---
+//  1. RUTE PUBLIK 
 Route::middleware('throttle:60,1')->group(function () {
+    
+    Route::get('/auth/github', function () {
+        return Http::get(env('AUTH_SERVICE_URL') . '/api/login/github')->json();
+    });
+
+    // Proxy ke Field Servic
     Route::get('/fields', function () {
         $response = Http::get(env('FIELD_SERVICE_URL') . '/api/fields');
         return response()->json($response->json(), $response->status());
     });
 });
 
-// --- 3. PROXY KE BOOKING SERVICE ---
-Route::post('/bookings', function (Request $request) {
-    $response = Http::post(env('BOOKING_SERVICE_URL') . '/api/bookings', $request->all());
-    return response()->json($response->json(), $response->status());
+//2. RUTE TERPROTEKSI
+Route::middleware(['throttle:60,1', 'jwt.gateway'])->group(function () {
+    
+    // Proxy ke Booking Service
+    Route::post('/bookings', function (Request $request) {
+        $response = Http::post(env('BOOKING_SERVICE_URL') . '/api/bookings', $request->all());
+        return response()->json($response->json(), $response->status());
+    });
+
+    // Proxy ke Auth Service untuk Logout/Me
+    Route::get('/auth/me', function (Request $request) {
+        $response = Http::withHeaders([
+            'Authorization' => $request->header('Authorization')
+        ])->get(env('AUTH_SERVICE_URL') . '/api/me');
+        return response()->json($response->json(), $response->status());
+    });
 });
